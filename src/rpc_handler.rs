a12e309f45ba3ee441e2701e9bb34f8269ac3d8d
Michael Bruty <57625243+mbruty@users.noreply.github.com>
2022-10-06 15:53:22 +0100

    Merge issue

--- src/rpc_handler.rs
+++ src/rpc_handler.rs
@@ -4,16 +4,47 @@ use std::time::Instant;
 use crate::{get_strategy_for, Languages};
 use crate::utils::{exec_command, get_stats, process_stats};
 use crate::strategies::Strategy;
+use serde::{Serialize, Deserialize};
 
-pub fn handle(code: &str, language: Languages) -> String {
+#[derive(Deserialize)]
+struct Instruction {
+    code: String,
+    test: String
+}
+
+#[derive(Serialize)]
+struct Stat {
+    cpu: String,
+    mem: String
+}
+
+#[derive(Serialize)]
+struct CodeResult {
+    output: String,
+    stats: Vec<Stat>,
+    execution_time_ms: u32,
+    error_text: String
+}
+pub fn handle(instruction: &str, language: Languages) -> String {
     // ToDo: Read this with commandline args
     let ctx: Box<dyn Strategy> = get_strategy_for(language);
 
     // Temp: Just build without validation
-    ctx.build(code);
+    let now = Instant::now();
+    let deserialized: Instruction = serde_json::from_str(&instruction).unwrap();
+    if let Err(e) = ctx.build(&*deserialized.code) {
+        let code_result = CodeResult{
+            output: "".parse().unwrap(),
+            stats: Vec::new(),
+            execution_time_ms: 0,
+            error_text: e
+        };
 
-    // ToDo: Validate build
-    //let build = ctx.build(code);
+        return serde_json::to_string(&code_result).unwrap();
+    }
+    ctx.setup_tests(&*deserialized.test);
+    let elapsed = now.elapsed();
+    println!("[.{}] build took: {:.2?}", ctx.get_queue_name(), elapsed);
 
     let command = ctx.get_command();
     let shared_command = Arc::new(Mutex::new(command));
@@ -26,22 +57,6 @@ pub fn handle(code: &str, language: Languages) -> String {
     let output = Arc::clone(&shared_output);
     let stats = Arc::clone(&shared_stats);
     let time = Arc::clone(&shared_time);
-
-    let code_thread = thread::spawn(move || {
-        let ctx_1: Box<dyn Strategy> = get_strategy_for(language);
-
-        // Clone the objects we need
-        // Lock the items we need
-        let mut output_mutex = output.lock().unwrap();
-        let mut time_mutex = time.lock().unwrap();
-
-        let now = Instant::now();
-        *output_mutex = ctx_1.run();
-        let elapsed = now.elapsed();
-
-        *time_mutex = format!("{:.2?}", elapsed.as_millis());
-    });
-
 
     let stats_thread = thread::spawn(move || {
 
@@ -81,6 +96,21 @@ pub fn handle(code: &str, language: Languages) -> String {
         *stats_mutex = results.join("\n")
     });
 
+    let code_thread = thread::spawn(move || {
+        let ctx_1: Box<dyn Strategy> = get_strategy_for(language);
+
+        // Clone the objects we need
+        // Lock the items we need
+        let mut output_mutex = output.lock().unwrap();
+        let mut time_mutex = time.lock().unwrap();
+
+        let now = Instant::now();
+        *output_mutex = ctx_1.run();
+        let elapsed = now.elapsed();
+
+        *time_mutex = format!("{:.2?}", elapsed.as_millis());
+    });
+
     // Wait for both of the threads to finish
     code_thread.join().expect("TODO: panic message");
     stats_thread.join().expect("TODO: panic message");
@@ -89,51 +119,25 @@ pub fn handle(code: &str, language: Languages) -> String {
     let output_copy = Arc::clone(&shared_output);
     let stats_copy = Arc::clone(&shared_stats);
     let time_copy = Arc::clone(&shared_time);
-    let mut final_output = output_copy.lock().unwrap();
+    let final_output = output_copy.lock().unwrap();
     let final_stats = stats_copy.lock().unwrap();
     let final_time = time_copy.lock().unwrap();
 
-    // Sanitise the final output
-    *final_output = (*final_output.replace("\"","\\\"")).parse().unwrap();
-
-    // Create the json response
-    let lines = (*final_output.as_str().split("\n").collect::<Vec<&str>>()).to_owned();
-    let mut output_obj = String::new();
-
-    // Add { "output": [
-    output_obj.push_str("{\"output\": [");
-
-    // Add each line to the output
-    for &line in &lines {
-        output_obj.push_str(&*format!("\"{}\",", line));
-    }
-
-    output_obj.pop();
-
-    // Add closing ]
-    output_obj.push_str("],");
-
-    // Add "stats": [
-    output_obj.push_str("\"stats\": [");
-
     let stat_lines = (*final_stats.as_str().split("\n").collect::<Vec<&str>>()).to_owned();
-
-    // Add each stat to the output { "cpu": %, "mem": mb }
+    let mut stats: Vec<Stat> = Vec::new();
     for &line in &stat_lines {
         let split = line.split(",").collect::<Vec<&str>>();
         if split.len() == 2 {
-            output_obj.push_str(&*format!("{{\"cpu\": \"{}\", \"mem\": \"{}\"}},", split[0], split[1]));
+            stats.push(Stat { cpu: split[0].parse().unwrap(), mem: split[1].parse().unwrap() });
         }
     }
-    output_obj.pop();
 
-    // Add closing ] to the string and opening "execution_time" :
-    output_obj.push_str("], \"execution_time_ms\" : ");
+    let code_result = CodeResult{
+        output: final_output.to_string(),
+        stats,
+        execution_time_ms: final_time.parse().unwrap(),
+        error_text: "".parse().unwrap()
+    };
 
-    // Add execution time to the string
-    output_obj.push_str(&*final_time);
-
-    // Close the final child object, and parent object
-    output_obj.push_str("}");
-    return output_obj;
+    serde_json::to_string(&code_result).unwrap()
 }
