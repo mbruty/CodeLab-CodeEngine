bacd7296944ff3d1b68c25804efdaf4e9899ba8b
Michael Bruty <57625243+mbruty@users.noreply.github.com>
2022-10-14 12:15:45 +0100

    Update dotnet_strategy.rs

--- src/strategies/dotnet_strategy.rs
+++ src/strategies/dotnet_strategy.rs
@@ -1,96 +1,10 @@
 use crate::strategies::Strategy;
-use crate::utils::{delete_file, exec_command_output, write_to_file};
+use crate::utils::{exec_command_output, write_to_file};
 
 pub struct DotnetStrategy;
 impl Strategy for DotnetStrategy {
-    fn warm_up(&self) {
-        println!("[.DOTNET] Warming up");
-        const CSPROJ: &str = "
-<Project Sdk=\"Microsoft.NET.Sdk\">
-  <PropertyGroup>
-    <OutputType>Exe</OutputType>
-    <TargetFramework>net6.0</TargetFramework>
-    <ImplicitUsings>enable</ImplicitUsings>
-    <Nullable>enable</Nullable>
-    <IsPackable>false</IsPackable>
-    <GenerateProgramFile>false</GenerateProgramFile>
-  </PropertyGroup>
-  <ItemGroup>
-    <PackageReference Include=\"nunit\" Version=\"3.13.3\" />
-    <PackageReference Include=\"Microsoft.NET.Test.Sdk\" Version=\"17.3.2\" />
-    <PackageReference Include=\"NUnitLite\" Version=\"3.13.3\" />
-    <PackageReference Include=\"NUnit3TestAdapter\" Version=\"4.2.1\" />
-  </ItemGroup>
-</Project>
-";
-        // Write the csproj to fs
-        write_to_file(CSPROJ, "Application.csproj");
-
-        const TEST_RUNNER: &str = "
-using NUnitLite;
-using System.Reflection;
-
-namespace program;
-public class Program
-{
-    public static int Main()
-    {
-        return new AutoRun(Assembly.GetExecutingAssembly())
-                       .Execute(new String[] { \"/test:program\" });
-    }
-}
-";
-        // Write the test runner code to the file system
-        write_to_file(TEST_RUNNER, "Program.cs");
-
-        const DUMMY_TESTS: &str = "
-using NUnit.Framework;
-
-namespace program;
-
-[TestFixture]
-public class TestRunner
-{
-    private Solution? _solution;
-
-    [SetUp]
-    public void SetUp()
-    {
-        _solution = new Solution();
-    }
-
-    [Test]
-    public void DummyTest()
-    {
-        Assert.AreEqual(0, _solution?.Solve());
-    }
-}";
-
-        // Write the dummy tests to the fs
-        self.setup_tests(DUMMY_TESTS);
-
-        const DUMMY_CODE: &str = "
-namespace program;
-public class Solution
-{
-    public int Solve()
-    {
-        // Your code here
-        return 0;
-    }
-}";
-        // Write the dummy code to the FS
-        self.build(DUMMY_CODE).expect("TODO: panic message");
-
-        // Clean up the files that don't need to still be there
-        delete_file("Solution.cs");
-        delete_file("UnitTests.cs");
-    }
-
     fn build(&self, code: &str) -> Result<String, String> {
-
         // Write the program to fs
-        write_to_file(code, "Solution.cs");
         let output = exec_command_output("dotnet", Vec::from(["build", "--configuration", "Release"]));
         let stdout = String::from_utf8(output.stdout).expect("");
 
