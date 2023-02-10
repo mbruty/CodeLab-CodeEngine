d3a4ffc0c4384c5dd10e2f590f714060dae7ac70
Michael Bruty <57625243+mbruty@users.noreply.github.com>
2023-02-10 13:03:47 +0000

    Override default test output to stderr

--- warmup-code/dotnet/Program.cs
+++ warmup-code/dotnet/Program.cs
@@ -1,11 +1,51 @@
+using Newtonsoft.Json.Linq;
+using NUnit.Common;
 using NUnitLite;
 using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+
 namespace program;
+
+class Writer : ExtendedTextWriter
+{
+    public override Encoding Encoding => throw new NotImplementedException();
+
+    public override void Write(ColorStyle style, string value)
+    {
+        Console.Error.Write(value);
+    }
+
+    public override void WriteLabel(string label, object option)
+    {
+        Console.Error.Write(label + '\t' + option);
+    }
+
+    public override void WriteLabel(string label, object option, ColorStyle valueStyle)
+    {
+        Console.Error.Write(label + '\t' + option);
+    }
+
+    public override void WriteLabelLine(string label, object option)
+    {
+        Console.Error.WriteLine(label + '\t' + option);
+    }
+
+    public override void WriteLabelLine(string label, object option, ColorStyle valueStyle)
+    {
+        Console.Error.WriteLine(label + '\t' + option);
+    }
+
+    public override void WriteLine(ColorStyle style, string value)
+    {
+        Console.Error.WriteLine(value);
+    }
+}
 public class Program
 {
     public static int Main()
     {
         return new AutoRun(Assembly.GetExecutingAssembly())
-                       .Execute(new String[] { "/test:program" });
+                       .Execute(new string[] { "/test:program" }, new Writer(), null);
     }
 }
