2b4d422491841c913e3a5b7be0b902425cf7fb0c
Michael Bruty <57625243+mbruty@users.noreply.github.com>
2023-02-15 11:53:50 +0000

    Add matplotlib support

--- docker/python.dockerfile
+++ docker/python.dockerfile
@@ -10,5 +10,7 @@ RUN apt-get install python3.8 -y
 RUN apt install python3-pip	-y
 RUN pip install numpy
 RUN pip install pandas
+RUN pip install matplotlib
+COPY ./setup-code/python ./unsafe
 COPY ./target/release/code_engine_rust .
 ENTRYPOINT ["./code_engine_rust", "--language=python"]
---
+++ setup-code/python/matplotlib_mocks.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import io
+import base64
+
+def show():
+    bytes = io.BytesIO()
+    plt.savefig(bytes, format='jpg')
+    bytes.seek(0)
+    base64_encoded = base64.b64encode(bytes.read()).decode()
+    print("<%media> data:image/jpeg;base64," + base64_encoded)
+
+plt.imshow = show
